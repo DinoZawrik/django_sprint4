b1e97e2d2afcf1cf4ecced684a7ea25d223001b1
DinoZawrik <79103321517@yandex.ru>
2025-06-03 01:29:36 +0300

    xxx

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
 # Для отдачи медиа-файлов в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                           document_root=settings.MEDIA_ROOT)
+            document_root=settings.MEDIA_ROOT)
 
 handler500 = "blog.views.server_error"
 
